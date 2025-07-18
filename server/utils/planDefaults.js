3ea45a44cd91b412dc8673ea5656ccc91bc84d99
ufdevs <ramesh_25s12res130@iitp.ac.in>
2025-07-18 11:52:22 +0530

    modification

--- server/utils/planDefaults.js
+++ server/utils/planDefaults.js
@@ -23,7 +23,7 @@ const PLAN_DEFAULTS = {
         },
     },
     premium: {
-        price: 100,
+        price: 9900,
         billingInterval: 'monthly',
         limits: {
             uploads: 100,
@@ -45,7 +45,7 @@ const PLAN_DEFAULTS = {
         billingInterval: 'monthly',
     },
     enterprise: {
-        price: 100,
+        price: 49900,
         billingInterval: 'monthly',
         limits: {
             uploads: 1000,
